1c48210101ad9fdbbfb2bffe32d7081c28d0e700
fsjohnhuang <fsjohnhuang@hotmail.com>
2026-09-04 18:05:31 +0800

    init vue example project

---
+++ examples/vue/src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <FitinCanvas :design-width="1920" :design-height="1080">
+    <FitinContainer
+      :design-width="220"
+      :design-height="800"
+      style="background: red; height: 100%"
+    >
+      123
+    </FitinContainer>
+  </FitinCanvas>
+</template>
