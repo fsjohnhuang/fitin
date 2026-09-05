61964d028d1e394ac25f8d4ec8dbd5b6abaf456f
john huang <fsjohnhuang@hotmail.com>
2026-09-05 10:00:53 +0800

    setup @fitin/react subrepo

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,27 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+       name: 'Fitin',
+      fileName: (format) => `fitin.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+})
