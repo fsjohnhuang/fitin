8a3814fd2f19ddf56af7e5ca36e5d94c3a8b1419
john huang <fsjohnhuang@hotmail.com>
2026-09-06 10:08:29 +0800

    get FitinContainer done

--- packages/react/src/components/FitinContainer.tsx
+++ packages/react/src/components/FitinContainer.tsx
@@ -1,4 +1,6 @@
-import { useRef, type CSSProperties, type ReactNode } from "react";
+import type { IResizeEvent } from "@fitin/core";
+import { useEffect, useMemo, useRef, type CSSProperties, type ReactNode } from "react";
+import { useFitin } from "./hooks";
 
 type FitinContainerProps = {
   designWidth: number;
@@ -10,13 +12,67 @@ type FitinContainerProps = {
   children?: ReactNode;
 };
 
+function getFitinStyle(event: IResizeEvent, props: FitinContainerProps): CSSProperties {
+  if (props.transformOriginX || props.transformOriginY) {
+    let transform: string;
+    let transformOrigin: string;
+    if (props.transformOriginX && props.transformOriginY) {
+      const res = event.transformXY(
+        props.transformOriginX,
+        props.transformOriginY,
+      );
+      transform = res.transform;
+      transformOrigin = res.transformOrigin;
+    } else if (props.transformOriginX) {
+      const res = event.transformX(props.transformOriginX);
+      transform = res.transform;
+      transformOrigin = res.transformOrigin;
+    } else {
+      const res = event.transformY(props.transformOriginY!);
+      transform = res.transform;
+      transformOrigin = res.transformOrigin;
+    }
+    return {
+      position: "absolute",
+      width: props.designWidth,
+      height: props.designHeight,
+      transform: transform,
+      transformOrigin: transformOrigin,
+    };
+  } else {
+    return {
+      width: event.scaleX * props.designWidth,
+      height: event.scaleY * props.designHeight,
+    };
+  }
+}
+
 export default function FitinContainer(props: FitinContainerProps) {
     const fitinStyle = useRef<CSSProperties>({});
+    const affectScope = useMemo(() =>
+        props.transformOriginX || props.transformOriginY
+            ? "affect-descendant"
+            : "affect-self"
+    , [props.transformOriginX, props.transformOriginY]);
+
+    const resizeObservable = useFitin();
+    useEffect(() => {
+        if (resizeObservable) {
+            const subscription = resizeObservable.subscribe(
+            (event: IResizeEvent | undefined) => {
+                if (event) {
+                    fitinStyle.current = getFitinStyle(event, props);
+                }
+            },
+            );
+            return () => subscription.unsubscribe();
+        }
+    }, []);
 
     return (
     <section
         role="fitin-container"
-        data-scope="affectScope"
+        data-scope={affectScope}
         style={{...props.style, ...fitinStyle.current}}
         className={props.class}
     >
--- packages/react/src/components/hooks.ts
+++ packages/react/src/components/hooks.ts
@@ -1,5 +1,29 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import type { Observable } from "rxjs";
 import type { IResizeEvent } from "@fitin/core";
+import { logE, logW } from "./logging";
 
 export const CanvasContext = createContext<Observable<undefined | IResizeEvent >>(undefined);
+
+const ContainerContext = createContext<string[]>([]);
+
+export function useFitin(
+  resizeHandler?: (event: undefined | IResizeEvent) => void,
+): undefined | Observable<undefined | IResizeEvent> {
+  const resizeObservable = useContext(CanvasContext);
+  if (resizeObservable) {
+    const paths = useContext(ContainerContext);
+    if (paths.find((path) => path === "affect-descendant")) {
+      logW("There's an ancestor FitinContainer resizing all descendants");
+    }
+
+    useEffect(() => {
+      const subscription = resizeObservable.subscribe(resizeHandler);
+      return () => subscription.unsubscribe();
+    }, [])
+  } else {
+    logE("A FitinContainer must be used within a FitinCanvas context");
+  }
+
+  return resizeObservable;
+}
